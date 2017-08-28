ace0b1ddb3937059ba7802821a834fef224e5991
Sławomir Paszko <s.paszko@softint.pl>
2017-08-28 11:29:49 +0200

    Adde C and matlab file exporting.

--- Makefile
+++ Makefile
@@ -1,3 +1,5 @@
 all : wav2c
-wa2c: main.c
+clean:
+	rm -rf wav2c
+wav2c: main.c
 	gcc main.c -o wav2c
--- main.c
+++ main.c
@@ -21,6 +21,10 @@ typedef struct {
     uint32_t Subchunk2Size;
 } __attribute__((packed)) sWavHeader;
 
+uint32_t numOfSamples = 0;
+uint16_t * leftChannel = NULL;
+uint16_t * rightChannel = NULL;
+
 
 void swapEndiannes(uint32_t * value)
 {
@@ -42,32 +46,123 @@ void printfU32String(uint32_t array)
 
 void printfHeader(sWavHeader * header)
 {
-    printf("WAV HEADER.\n");
-    printf("--------------------------------------.\n");
-    printf("ChunkID : ");
-    swapEndiannes(&header->ChunkID);
-    printfU32String(header->ChunkID);
-    printf("ChunkSize : %d.\n",header->ChunkSize);
-    printf("Format : ");
-    swapEndiannes(&header->Format);
-    printfU32String(header->Format);
-    /// fmt
-    printf("Subchunk1ID : ");
-    swapEndiannes(&header->Subchunk1ID);
-    printfU32String(header->Subchunk1ID);
-    printf("Subchunk1Size : %d.\n",header->Subchunk1Size);
-    printf("AudioFormat : %d.\n",header->AudioFormat);
-    printf("NumChannels : %d.\n",header->NumChannels);
-    printf("SampleRate : %d.\n",header->SampleRate);
-    printf("ByteRate : %d.\n",header->ByteRate);
-    printf("BlockAlign : %d.\n",header->BlockAlign);
-    printf("BitsPerSample : %d.\n",header->BitsPerSample);
-    /// data
-    printf("Subchunk2ID : ");
-    swapEndiannes(&header->Subchunk2ID);
-    printfU32String(header->Subchunk2ID);
-    printf("Subchunk2Size : %d.\n",header->Subchunk2Size);
-    printf("--------------------------------------.\n");
+  printf("WAV HEADER.\n");
+  printf("--------------------------------------.\n");
+  printf("ChunkID : ");
+  swapEndiannes(&header->ChunkID);
+  printfU32String(header->ChunkID);
+  printf("ChunkSize : %d.\n", header->ChunkSize);
+  printf("Format : ");
+  swapEndiannes(&header->Format);
+  printfU32String(header->Format);
+  /// fmt
+  printf("Subchunk1ID : ");
+  swapEndiannes(&header->Subchunk1ID);
+  printfU32String(header->Subchunk1ID);
+  printf("Subchunk1Size : %d.\n", header->Subchunk1Size);
+  if(header->AudioFormat == 1)
+  {
+    printf("AudioFormat : PCM.\n");
+  }
+  else
+  {
+    printf("AudioFormat : Compression.\n");
+  }
+  printf("NumChannels : %d.\n", header->NumChannels);
+  printf("SampleRate : %d.\n", header->SampleRate);
+  printf("ByteRate : %d.\n", header->ByteRate);
+  printf("BlockAlign : %d.\n", header->BlockAlign);
+  printf("BitsPerSample : %d.\n", header->BitsPerSample);
+  /// data
+  printf("Subchunk2ID : ");
+  swapEndiannes(&header->Subchunk2ID);
+  printfU32String(header->Subchunk2ID);
+  printf("Subchunk2Size : %d.\n", header->Subchunk2Size);
+  printf("--------------------------------------.\n");
+}
+
+void fprintfChannel(FILE *pFile , uint16_t * array, uint32_t size)
+{
+  for (uint32_t i = 0; i<size; ++i)
+  {
+    fprintf(pFile, "0x%04x,\n",array[i]);
+  }
+}
+
+void fprintfChannelMatlab(FILE *pFile , uint16_t * array, uint32_t size)
+{
+  for (uint32_t i = 0; i<size; ++i)
+  {
+    fprintf(pFile, "0x%04x ",array[i]);
+  }
+}
+
+void readWavData(int fileId, sWavHeader * header)
+{
+  numOfSamples = header->Subchunk2Size / (header->NumChannels * header->BitsPerSample/8);
+
+  leftChannel = malloc(sizeof(uint16_t) * numOfSamples);
+  if (header->NumChannels > 1)
+  {
+    rightChannel = malloc(sizeof(uint16_t) * numOfSamples);
+  }
+
+  /// read all music data
+  for (uint32_t i = 0; i<numOfSamples; ++i)
+  {
+    read(fileId, &leftChannel[i], sizeof(uint16_t));
+    if(header->NumChannels > 1)
+    {
+      read(fileId, &rightChannel[i], sizeof(uint16_t));
+    }
+  }
+}
+
+void writeAsCFile(uint16_t *leftChannel, uint16_t *rightChannel,  sWavHeader * header)
+{
+  FILE * pFile;
+  pFile = fopen("sound.c","w");
+
+  /// header
+  fprintf(pFile, "#include <stdint.h>\n");
+
+  /// printf left Channel
+  fprintf(pFile, "uint16_t leftChannel[] = {\n");
+  fprintfChannel(pFile, leftChannel, numOfSamples);
+  fprintf(pFile, "};\n");
+
+  if(header->NumChannels > 1)
+  {
+    /// printf right Channel
+    fprintf(pFile, "uint16_t rightChannel[] = {\n");
+    fprintfChannel(pFile, rightChannel, numOfSamples);
+    fprintf(pFile, "};\n");
+  }
+  fclose(pFile);
+}
+
+void writeAsMatlabFile(uint16_t *leftChannel, uint16_t *rightChannel,  sWavHeader * header)
+{
+  FILE * pFile;
+  pFile = fopen("sound.m","w");
+
+  /// printf left Channel
+  fprintf(pFile, "leftChannel = [");
+  fprintfChannelMatlab(pFile, leftChannel, numOfSamples);
+  fprintf(pFile, "];");
+
+  if(header->NumChannels > 1)
+  {
+    /// printf right Channel
+    fprintf(pFile, "rightChannel = [");
+    fprintfChannelMatlab(pFile, rightChannel, numOfSamples);
+    fprintf(pFile, "];");
+    fprintf(pFile, "plot(rightChannel);\n");
+  }
+  fprintf(pFile, "plot(leftChannel);\n");
+  fprintf(pFile, "input(\"Wait for keypress\");\n");
+  fclose(pFile);
+
 }
 
 int main(int argc, char * argv[])
@@ -85,7 +180,11 @@ int main(int argc, char * argv[])
     int file = open(argv[1],O_RDONLY);
     read(file, &wavHeader, sizeof(sWavHeader));
     printfHeader(&wavHeader);
+    readWavData(file, &wavHeader);
     close(file);
+
+    writeAsCFile(leftChannel, rightChannel, &wavHeader);
+    writeAsMatlabFile(leftChannel, rightChannel, &wavHeader);
 
     return 0;
 }
